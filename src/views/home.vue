e66a310923f805d4ae0e756be72e3685245d6ef2
冯亚宁 <fengyaning@cnb.design>
2021-07-20 15:07:10 +0800

    fix: 不忽略大小写

---
+++ src/views/home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <h1>首页</h1>
+    <img :src="logoUrl" alt="vue">
+  </div>
+</template>
+
+<script>
+import { getExplorer } from '@/assets/scripts/tools'
+import logoUrl from '@/assets/images/logo.png'
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      logoUrl
+    }
+  },
+  created() {
+    this.getExplorer()
+  },
+  methods: {
+    // 获取当前浏览器信息
+    getExplorer() {
+      const info = getExplorer()
+      console.log(info)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+  padding: 30px;
+  text-align: center;
+}
+</style>
